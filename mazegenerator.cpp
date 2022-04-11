0d7a68e842934662eb14c523ffbfc4e3f4967b73
MaksKuchuk <94689394+MaksKuchuk@users.noreply.github.com>
2022-04-12 01:04:59 +1000

    fix some bugs

--- mazegenerator.cpp
+++ mazegenerator.cpp
@@ -4,22 +4,22 @@
 #include <time.h>
 using namespace std;
 
-class Labyrinth{
+class Labyrinth {
 public:
-    Labyrinth(){}
+    Labyrinth() {}
     static vector < vector <int> > create_labyrinth(int w, int h);
     static int* read_labyrinth(vector < vector <int> > a_double);
 };
 
-vector < vector <int> > Labyrinth::create_labyrinth(int w, int h){
-    vector < vector <int> > array(h , vector <int> (w) );
+vector < vector <int> > Labyrinth::create_labyrinth(int w, int h) {
+    vector < vector <int> > array(h, vector <int>(w));
     vector< pair<int, int> > free_coordinates;
-    for (int i = 0; i < h; ++i){
-        for (int j = 0; j < w; ++j){
-            if (i == 0 or i == array.size()-1 or j == 0 or j == array[0].size()-1)
+    for (int i = 0; i < h; ++i) {
+        for (int j = 0; j < w; ++j) {
+            if (i == 0 or i == array.size() - 1 or j == 0 or j == array[0].size() - 1)
                 array[i][j] = 3;
             else {
-                free_coordinates.emplace_back(i,j);
+                free_coordinates.emplace_back(i, j);
                 array[i][j] = 0;
             }
         }
@@ -52,41 +52,41 @@ vector < vector <int> > Labyrinth::create_labyrinth(int w, int h){
     }
     //--------------------------------------------------------------------------------------
     //------------------------Основная часть алгоритма--------------------------------------
-    while(!pm.empty())
+    while (!pm.empty())
     {
         srand(time(0));
         int rng = 0 + rand() % (pm.size());
 
-        if (array[pm[rng].first][pm[rng].second] == 2){
+        if (array[pm[rng].first][pm[rng].second] == 2) {
             pm.erase(pm.begin() + rng);
             continue;
         }
 
         array[pm[rng].first][pm[rng].second] = 5;
 
-        if (array[pm[rng].first + 1][pm[rng].second] != 3 and array[pm[rng].first + 1][pm[rng].second] != 5 and array[pm[rng].first + 1][pm[rng].second] != 2){
+        if (array[pm[rng].first + 1][pm[rng].second] != 3 and array[pm[rng].first + 1][pm[rng].second] != 5 and array[pm[rng].first + 1][pm[rng].second] != 2) {
             array[pm[rng].first + 1][pm[rng].second] += 1;
             pm.emplace_back(pm[rng].first + 1, pm[rng].second);
         }
 
-        if (array[pm[rng].first - 1][pm[rng].second] != 3 and array[pm[rng].first - 1][pm[rng].second] != 5 and array[pm[rng].first - 1][pm[rng].second] != 2){
+        if (array[pm[rng].first - 1][pm[rng].second] != 3 and array[pm[rng].first - 1][pm[rng].second] != 5 and array[pm[rng].first - 1][pm[rng].second] != 2) {
             array[pm[rng].first - 1][pm[rng].second] += 1;
             pm.emplace_back(pm[rng].first - 1, pm[rng].second);
         }
 
-        if (array[pm[rng].first][pm[rng].second + 1] != 3 and array[pm[rng].first][pm[rng].second + 1] != 5 and array[pm[rng].first][pm[rng].second + 1] != 2){
+        if (array[pm[rng].first][pm[rng].second + 1] != 3 and array[pm[rng].first][pm[rng].second + 1] != 5 and array[pm[rng].first][pm[rng].second + 1] != 2) {
             array[pm[rng].first][pm[rng].second + 1] += 1;
-            pm.emplace_back(pm[rng].first , pm[rng].second + 1);
+            pm.emplace_back(pm[rng].first, pm[rng].second + 1);
         }
 
-        if (array[pm[rng].first][pm[rng].second - 1] != 3 and array[pm[rng].first][pm[rng].second - 1] != 5 and array[pm[rng].first][pm[rng].second - 1] != 2){
+        if (array[pm[rng].first][pm[rng].second - 1] != 3 and array[pm[rng].first][pm[rng].second - 1] != 5 and array[pm[rng].first][pm[rng].second - 1] != 2) {
             array[pm[rng].first][pm[rng].second - 1] += 1;
-            pm.emplace_back(pm[rng].first , pm[rng].second - 1);
+            pm.emplace_back(pm[rng].first, pm[rng].second - 1);
         }
         pm.erase(pm.begin() + rng);
     }
-    for (int i = 0; i < array.size(); i++){
-        for (int j = 0; j < array[0].size() ; j++){
+    for (int i = 0; i < array.size(); i++) {
+        for (int j = 0; j < array[0].size(); j++) {
             if (array[i][j] == 3 or array[i][j] == 2)
                 array[i][j] = 1;
             else
@@ -97,15 +97,13 @@ vector < vector <int> > Labyrinth::create_labyrinth(int w, int h){
     return array;
 }
 
-int* Labyrinth::read_labyrinth(vector < vector <int> > a_double){
+int* Labyrinth::read_labyrinth(vector < vector <int> > a_double) {
     vector<int> result;
-    int *res = new int[a_double[0].size() * a_double.size()+ 2];
-    res[0] = a_double[0].size();
-    res[1] = a_double.size();
+    int* res = new int[a_double[0].size() * a_double.size()];
 
-    int t = 2;
-    for (int i = 0; i < a_double.size(); i++){
-        for (int j = 0; j < a_double[0].size() ; j++){
+    int t = 0;
+    for (int i = 0; i < a_double.size(); i++) {
+        for (int j = 0; j < a_double[0].size(); j++) {
             res[t] = a_double[i][j];
             t += 1;
         }
